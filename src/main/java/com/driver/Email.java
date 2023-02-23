2b268b545686d4b8e36156f5fc68cf50a9c28e87
somuip <117620797+somuip@users.noreply.github.com>
2023-02-23 13:10:26 +0530

    first attempt

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import com.google.common.base.Strings;
+
 public class Email {
 
     private String emailId;
@@ -9,6 +11,7 @@ public class Email {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
+
 
     public String getEmailId() {
         return emailId;
@@ -25,5 +28,38 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(password.equals(oldPassword)){
+            if(isValid(newPassword)){
+                this.password = newPassword;
+                System.out.println("Password has been changed successfully");
+            }
+            else{
+                System.out.println("Password does not meet the requirements");
+            }
+        }
+        else{
+            System.out.println("The given password does not match with the password");
+        }
+    }
+    public boolean isValid(String password){
+        boolean capitalLetter = false;
+        boolean smallLetter = false;
+        boolean digit = false;
+        boolean special = false;
+
+        if(password.length() < 8){
+            return false;
+        }
+
+        for(int i=0;i<password.length();i++){
+            char c = password.charAt(i);
+            if(c >= 'A' && c <= 'Z') capitalLetter = true;
+            else if(c >= 'a' && c <= 'z') smallLetter = true;
+            else if(c >= '0' && c <= '9') digit = true;
+            else special = true;
+        }
+
+        if(capitalLetter && smallLetter && digit && special) return true;
+        return false;
     }
 }
